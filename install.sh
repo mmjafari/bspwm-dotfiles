bb07a81c91dc86d2eb527e2568069f4a62436e27
mmjafari <mmjafari@localhost.localdomain>
2018-01-05 17:12:16 +0900

    dot  files

---
+++ install.sh
@@ -0,0 +1,9 @@
+mkdir ~/.config/sxhkd
+cp Files/sxhkdrc ~/.config/sxhdk/
+mkdir ~/.config/bspwm
+cp Files/bspwmrc ~/.config/bspwm/
+mkdir ~/.config/lxterminal
+cp Files/lxterminal.conf ~/.config/lxterminal/
+mkdir ~/.config/tint2
+cp Files/tint2rc ~/.config/tint2/
+
